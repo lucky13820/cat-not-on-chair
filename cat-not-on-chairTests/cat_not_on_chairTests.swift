b66a04d3db70bba940b814d39dc9efe126a0bc5b
Ryan Yao <hello@ryanyao.design>
2025-04-07 15:36:54 +0800

    Initial Commit

---
+++ cat-not-on-chairTests/cat_not_on_chairTests.swift
@@ -0,0 +1,17 @@
+//
+//  cat_not_on_chairTests.swift
+//  cat-not-on-chairTests
+//
+//  Created by Ryan Yao on 2025-04-07.
+//
+
+import Testing
+@testable import cat_not_on_chair
+
+struct cat_not_on_chairTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
